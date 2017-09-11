2c111d8b12a87639753177ebf786cba299f8bad8
norbert-berenyi <norbert-berenyi@users.noreply.github.com>
2017-09-12 00:09:23 +0200

    Mobile support

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,51 +4,53 @@
 
 	<div class="container">
 
-		<br>
+		<section class="section">
 
-		<div class="content is-clearfix">
-			
-			<h1 class="is-pulled-left">Ads:</h1>
-
-			@if(url()->current() != url('/'))
-				<a class="button is-primary is-pulled-right" href="{{ url('/reports/download/' . $link) }}">Download</a>
-			@endif
-
-		</div>
-
-		<table class="table is-bordered is-fullwidth">
-			
-			<thead>
+			<div class="content is-clearfix">
 				
-				<tr>
+				<h1 class="is-pulled-left">Ads:</h1>
+
+				@if(url()->current() != url('/'))
+					<a class="button is-primary is-pulled-right" href="{{ url('/reports/download/' . $link) }}">Download</a>
+				@endif
+
+			</div>
+
+			<table class="table is-bordered is-fullwidth">
+				
+				<thead>
 					
-					<th>#</th>
-					<th>Address</th>
-					<th>Price</th>
-					<th>Area</th>
-
-				</tr>
-
-			</thead>
-
-			<tbody>
-				
-				@foreach($ads as $key => $ad)
-
 					<tr>
-
-						<td>{{ $key }}</td>
-						<td><a href="{{ $ad->link }}" target="_blank">{{ $ad->address }}</a></td>
-						<td>{{ $ad->price }}</td>
-						<td>{{ $ad->size }}</td>
+						
+						<th>#</th>
+						<th>Address</th>
+						<th>Price</th>
+						<th>Area</th>
 
 					</tr>
 
-				@endforeach
+				</thead>
 
-			</tbody>
+				<tbody>
+					
+					@foreach($ads as $key => $ad)
 
-		</table>
+						<tr>
+
+							<td>{{ $key }}</td>
+							<td><a href="{{ $ad->link }}" target="_blank">{{ $ad->address }}</a></td>
+							<td>{{ number_format($ad->price) }}</td>
+							<td>{{ $ad->size }}</td>
+
+						</tr>
+
+					@endforeach
+
+				</tbody>
+
+			</table>
+
+		</section>
 
 	</div>
 
