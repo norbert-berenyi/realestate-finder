da52d54cb7c316d9c283028d0f0a3ccbf8f854ed
norbert-berenyi <norbert-berenyi@users.noreply.github.com>
2017-09-14 22:10:38 +0200

    Added notification if there is nothing to show.

--- resources/views/adverts.blade.php
+++ resources/views/adverts.blade.php
@@ -18,74 +18,86 @@
 
 			</div>
 
-			<table class="table is-bordered is-fullwidth is-hidden-touch">
-				
-				<thead>
-					
-					<tr>
-						
-						<th>#</th>
-						<th>Address</th>
-						<th>Price</th>
-						<th>Area</th>
-						<th>Good</th>
+			@if(count($ads) > 0)
 
+				<table class="table is-bordered is-fullwidth is-hidden-touch">
+					
+					<thead>
+						
+						<tr>
+							
+							<th>#</th>
+							<th>Address</th>
+							<th>Price</th>
+							<th>Area</th>
+							<th>Good</th>
+
+						</tr>
+
+					</thead>
+
+					<tbody>
+
+						<tr v-for="(ad, index) in ads" :class="{'is-selected': ad.seen && ad.promising}">
+
+							<td>@{{ ad.id }}</td>
+							<td><a :href="ad.link" target="_blank" :class="{'has-text-info': ad.seen && !ad.promising}" @click="seen(index)">@{{ ad.address }}</a></td>
+							<td>@{{ ad.price }} Ft/hó</td>
+							<td>@{{ ad.size }} m<sup>2</sup></td>
+							<td>
+								<label class="checkbox">
+									<input type="checkbox" v-model="ad.promising" @click="update(index)">
+								</label>
+							</td>
+
+						</tr>
+
+					</tbody>
+
+				</table>
+
+				<table class="table is-bordered is-fullwidth is-hidden-desktop" v-for="(ad, index) in ads">
+
+					<tr :class="{'is-selected': ad.seen && ad.promising}">
+						<th width="20px">#</th>
+						<td>@{{ ad.id }}</td>
 					</tr>
 
-				</thead>
-
-				<tbody>
-
-					<tr v-for="(ad, index) in ads" :class="{'is-selected': ad.seen && ad.promising}">
-
-						<td>@{{ ad.id }}</td>
+					<tr :class="{'is-selected': ad.seen && ad.promising}">
+						<th>Address</th>
 						<td><a :href="ad.link" target="_blank" :class="{'has-text-info': ad.seen && !ad.promising}" @click="seen(index)">@{{ ad.address }}</a></td>
+					</tr>
+
+					<tr :class="{'is-selected': ad.seen && ad.promising}">
+						<th>Price</th>
 						<td>@{{ ad.price }} Ft/hó</td>
+					</tr :class="{'is-selected': ad.seen && ad.promising}">
+
+					<tr :class="{'is-selected': ad.seen && ad.promising}">
+						<th>Area</th>
 						<td>@{{ ad.size }} m<sup>2</sup></td>
+					</tr>
+
+					<tr :class="{'is-selected': ad.seen && ad.promising}">
+						<th>Good</th>
 						<td>
 							<label class="checkbox">
 								<input type="checkbox" v-model="ad.promising" @click="update(index)">
 							</label>
 						</td>
-
 					</tr>
 
-				</tbody>
+				</table>
 
-			</table>
+			@else
 
-			<table class="table is-bordered is-fullwidth is-hidden-desktop" v-for="(ad, index) in ads">
+				<div class="notification is-info">
+					
+					<p>There is nothing here.</p>
 
-				<tr :class="{'is-selected': ad.seen && ad.promising}">
-					<th width="20px">#</th>
-					<td>@{{ ad.id }}</td>
-				</tr>
+				</div>
 
-				<tr :class="{'is-selected': ad.seen && ad.promising}">
-					<th>Address</th>
-					<td><a :href="ad.link" target="_blank" :class="{'has-text-info': ad.seen && !ad.promising}" @click="seen(index)">@{{ ad.address }}</a></td>
-				</tr>
-
-				<tr :class="{'is-selected': ad.seen && ad.promising}">
-					<th>Price</th>
-					<td>@{{ ad.price }} Ft/hó</td>
-				</tr :class="{'is-selected': ad.seen && ad.promising}">
-
-				<tr :class="{'is-selected': ad.seen && ad.promising}">
-					<th>Area</th>
-					<td>@{{ ad.size }} m<sup>2</sup></td>
-				</tr>
-
-				<tr :class="{'is-selected': ad.seen && ad.promising}">
-					<th>Good</th>
-					<td>
-						<label class="checkbox">
-							<input type="checkbox" v-model="ad.promising" @click="update(index)">
-						</label>
-					</td>
-				</tr>
-
-			</table>
+			@endif
 
 		</section>
 
